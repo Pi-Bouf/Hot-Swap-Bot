bf6ac4ad99d57a8f151c178f382b18c8a95a5912
Pi-Bouf <pierre.bouffier@gmail.com>
2021-07-31 13:53:57 +0200

    Add Travis tests

--- tests/orders/TokenOrder.test.ts
+++ tests/orders/TokenOrder.test.ts
@@ -1,26 +1,31 @@
+import * as sinon from 'sinon';
+import { expect } from "chai";
 import {TokenOrder} from '../../src/server/orders/TokenOrder';
 import {Contracts} from '../../src/server/ethers/Contracts';
-
-import * as sinon from 'sinon';
 import {IToken} from '../../src/server/ethers/interface/IToken';
 
 describe('TokenOrder tests', function () {
-    const cakeAddress = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82';
-    let tokenOrder: TokenOrder = undefined;
+    const contracts = new Contracts(undefined);
     let contractsMock: sinon.SinonMock = undefined;
-    let token: IToken = {address: cakeAddress, decimals: 18, name: 'Pancake CAKE', symbol: 'CAKE'};
+
+    const cakeAddress = '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82';
+
+    let tokenOrder: TokenOrder = undefined;
+    let expectedToken: IToken = {address: cakeAddress, decimals: 18, name: 'Pancake CAKE', symbol: 'CAKE'};
 
     beforeEach('Init TokenOrder & attach mock', function () {
         tokenOrder = new TokenOrder(cakeAddress);
-        contractsMock = sinon.mock(new Contracts(undefined));
+        contractsMock = sinon.mock(contracts);
 
-        tokenOrder.attachContracts(<Contracts><any>contractsMock);
+        tokenOrder.attachContracts(contracts);
     });
 
     it('Create TokenOrder, attach contracts, retrieve infor', async function () {
-        contractsMock.expects('getTokenData').once().withArgs(cakeAddress).returns(token);
+        contractsMock.expects('getTokenData').once().withArgs(cakeAddress).returns(expectedToken);
 
         await tokenOrder.loadData();
+
+        expect(tokenOrder.token).is.eq(expectedToken);
 
         contractsMock.verify();
     });
